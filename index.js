64551ddcc4c98f88730e1275b1164c1f2d88e267
Koen Vendrik <k.vendrik@gmail.com>
2016-03-01 21:38:50 +0100

    Updates MONGOLAB env var

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ var server = require('./inc/server');
 
 server.start({
 	port: process.env.PORT,
-	mongoUri: process.env.POD_MONGODB || 'mongodb://localhost/url-shortner'
+	mongoUri: process.env.MONGOLAB_URI || 'mongodb://localhost/url-shortner'
 });
