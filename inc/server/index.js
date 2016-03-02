14eaad48118a569cff702dde79db2ff3d8732812
Koen Vendrik <k.vendrik@gmail.com>
2016-03-02 21:57:39 +0100

    Adds CORS allowance middleware

--- inc/server/index.js
+++ inc/server/index.js
@@ -6,13 +6,27 @@ let mongoose = require('mongoose'),
 let logger = require('./logger'),
 	routes = require('./routes');
 
+let middleware = {
+	init: function(){
+		app.use(this._allowCORS);
+		app.use(bodyParser.json());
+	},
+
+	_allowCORS: function(req, res, next){
+	    res.header('Access-Control-Allow-Origin', 'example.com');
+	    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+	    res.header('Access-Control-Allow-Headers', 'Content-Type');
+	    next();
+	}
+};
+
 module.exports = {
 
 	start: function(options){
 		options = options || {};
 		let port = options.port || 3000;
 
-		app.use(bodyParser.json());
+		middleware.init();
 		mongoose.connect(options.mongoUri);
 		
 		routes(app);
