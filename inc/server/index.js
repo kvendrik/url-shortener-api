b7dd8f2a6c0b0eeeab8f1a663df90e72681e861e
Koen Vendrik <k.vendrik@gmail.com>
2016-03-02 21:58:08 +0100

    Corrects allow-origin

--- inc/server/index.js
+++ inc/server/index.js
@@ -13,7 +13,7 @@ let middleware = {
 	},
 
 	_allowCORS: function(req, res, next){
-	    res.header('Access-Control-Allow-Origin', 'example.com');
+	    res.header('Access-Control-Allow-Origin', '*');
 	    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
 	    res.header('Access-Control-Allow-Headers', 'Content-Type');
 	    next();
